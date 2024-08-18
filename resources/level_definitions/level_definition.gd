1194ad8a1cc0499c17ded33c85bd5a3e6a87fceb
efevans <efevans1@gmail.com>
2024-08-18 13:42:31 -0700

    Add resources for levels and measureables

---
+++ resources/level_definitions/level_definition.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name LevelDefinition
+
+@export var id: String
+@export var left_side: Array[Measureable] = []
+@export var right_side: Array[Measureable] = []
---
+++ resources/measureables/measureable.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name Measureable
+
+@export var id: String
+@export var weight: int
