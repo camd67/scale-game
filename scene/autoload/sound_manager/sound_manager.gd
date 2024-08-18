07f1ae6314e06c9bb1b5cd8de3597df8446853d1
MrTekTiteToe <tektitetoe@gmail.com>
2024-08-18 14:46:07 -0700

    start the noise timer

--- scene/autoload/sound_manager/sound_manager.gd
+++ scene/autoload/sound_manager/sound_manager.gd
@@ -22,6 +22,7 @@ func _ready() -> void:
 	GameEvents.play_pressed.connect(on_play_pressed)
 	timer.timeout.connect(on_play_open_timeout)
 	random_noise_timer.timeout.connect(on_random_noise_timeout)
+	random_noise_timer.wait_time = MIN_NOISE_BUFFER + randi_range(0,NOISE_RANGE)
 	random_noise_timer.start()
 	
 	
