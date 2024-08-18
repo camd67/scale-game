b238415aa056666cd822cd5ce5285a7463c7b962
CamD67 <7773345+camd67@users.noreply.github.com>
2024-08-18 13:27:06 -0700

    Add OOB check for grabbed objects

--- scene/grabber/grabber.gd
+++ scene/grabber/grabber.gd
@@ -1,4 +1,5 @@
 extends Node3D
+class_name Grabber
 
 @export_group("Required Fields")
 # Camera for raycast stuff
--- scene/main/main.gd
+++ scene/main/main.gd
@@ -2,6 +2,11 @@ extends Node3D
 
 @onready var camera_animation_player: AnimationPlayer = $CameraAnimationPlayer
 
+@onready var out_of_bounds_checker: Area3D = $TableBoundaries/OutOfBoundsChecker
+@onready var grabber: Grabber = $Grabber
+
+@onready var table_left_spawn_point: Marker3D = $TableLeftSpawnPoint
+
 
 func _ready() -> void:
 	GameEvents.play_pressed.connect(func() -> void:
@@ -10,3 +15,19 @@ func _ready() -> void:
 	GameEvents.debug_instant_play.connect(func() -> void:
 		$MainCamera.global_transform = $InstantDebugCameraMarker.global_transform
 	)
+	out_of_bounds_checker.body_exited.connect(on_oob_checker_exited)
+
+
+func on_oob_checker_exited(body: Node3D) -> void:
+	if body is RigidBody3D:
+		# Play some audio here?
+		# "How'd you do that?"
+
+		# Freeze the body to stop all momentum
+		body.freeze = true
+		body.global_position = table_left_spawn_point.global_position
+		grabber.drop_item()
+		await get_tree().physics_frame
+		body.freeze = false
+	else:
+		body.global_position = table_left_spawn_point.global_position
