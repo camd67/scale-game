8c6607a56144158280d7036e89f988f9826f5c0a
efevans <efevans1@gmail.com>
2024-08-19 13:25:25 -0700

    Rotate beam on balancing

--- scene/scale/scale.gd
+++ scene/scale/scale.gd
@@ -1,6 +1,8 @@
 extends Node3D
 class_name Scale
 
+const PAN_DROP_POINT := .33
+
 @onready var left_pan: Area3D = $LeftPan
 var left_pan_weight: float = 0
 
@@ -27,20 +29,19 @@ func on_weight_submitted(result_callback: Callable) -> void:
 
 	var left_pan_mesh := $Visuals/PanR as Node3D
 	var right_pan_mesh := $Visuals/PanL as Node3D
-
-	var left_end_pos := Vector3(left_pan_mesh.global_position.x, starting_pan_height + pan_height_change, left_pan_mesh.global_position.z)
-	var right_end_pos := Vector3(right_pan_mesh.global_position.x, starting_pan_height - pan_height_change, right_pan_mesh.global_position.z)
+	var beam_mesh := $Visuals/Beam as Node3D
 
 	var tween_time := 3 if pan_difference != last_pan_difference else 0
 
 	var left_tween := create_tween().bind_node(left_pan_mesh)
 	var right_tween := create_tween().bind_node(right_pan_mesh)
+	var beam_tween := create_tween().bind_node(beam_mesh)
 
 	# Teeter the pans first to give them some motion
-	teeter_pans(left_tween, right_tween)
+	teeter_pans(left_tween, right_tween, beam_tween)
 
-	left_tween.tween_property(left_pan_mesh, "global_position", left_end_pos, 3).set_ease(Tween.EASE_OUT)
-	right_tween.tween_property(right_pan_mesh, "global_position", right_end_pos, 3).set_ease(Tween.EASE_OUT)
+	tween_balancing(left_tween, right_tween, beam_tween, pan_height_change, 3)
+
 	left_tween.tween_callback(func() -> void: result_callback.call())
 	last_pan_difference = pan_difference
 
@@ -50,18 +51,39 @@ func on_weight_submitted(result_callback: Callable) -> void:
 		left_tween.tween_callback(func() -> void: GameEvents.emit_incorrect_weight_submitted())
 
 
-func teeter_pans(left_tween: Tween, right_tween: Tween) -> void:
+func teeter_pans(left_tween: Tween, right_tween: Tween, beam_tween: Tween) -> void:
 	var left_pan_mesh := $Visuals/PanR as Node3D
 	var right_pan_mesh := $Visuals/PanL as Node3D
 
-	left_tween.tween_property(left_pan_mesh, "global_position", left_pan_mesh.global_position + Vector3.UP * .01, .4)
-	left_tween.tween_property(left_pan_mesh, "global_position", left_pan_mesh.global_position + Vector3.UP * -.01, .6)
-	left_tween.tween_property(left_pan_mesh, "global_position", left_pan_mesh.global_position + Vector3.UP * 0, .4)
+	var left_pan_relative_starting_height := left_pan_mesh.global_position.y - starting_pan_height
 
-	right_tween.tween_property(right_pan_mesh, "global_position", right_pan_mesh.global_position + Vector3.UP * -.01, .4)
-	right_tween.tween_property(right_pan_mesh, "global_position", right_pan_mesh.global_position + Vector3.UP * .01, .6)
-	right_tween.tween_property(right_pan_mesh, "global_position", right_pan_mesh.global_position + Vector3.UP * 0, .4)
+	tween_balancing(left_tween, right_tween, beam_tween, left_pan_relative_starting_height + .01, .4)
+	tween_balancing(left_tween, right_tween, beam_tween, left_pan_relative_starting_height + -.01, .6)
+	tween_balancing(left_tween, right_tween, beam_tween, left_pan_relative_starting_height + .0, .4)
 
+
+# Tweens the movement of pans and beam to end at a height relative to the neutral position
+func tween_balancing(left_pan_tween: Tween, right_pan_tween: Tween, beam_tween: Tween, \
+					left_relative_end_height: float, duration: float) -> void:
+
+	# Create tweens if called without
+	left_pan_tween = create_tween() if left_pan_tween == null else left_pan_tween
+	right_pan_tween = create_tween() if right_pan_tween == null else right_pan_tween
+	beam_tween = create_tween() if beam_tween == null else beam_tween
+
+	# Position Pans
+	var left_pan_mesh := $Visuals/PanR as Node3D
+	var right_pan_mesh := $Visuals/PanL as Node3D
+	left_pan_tween.tween_property(left_pan_mesh, "global_position:y", starting_pan_height + left_relative_end_height, duration)\
+					.set_ease(Tween.EASE_IN_OUT)
+	right_pan_tween.tween_property(right_pan_mesh, "global_position:y", starting_pan_height + -left_relative_end_height, duration)\
+					.set_ease(Tween.EASE_IN_OUT)
+
+	# Rotate beam
+	var beam_mesh := $Visuals/Beam as Node3D
+	var rads := -asin(left_relative_end_height / PAN_DROP_POINT)
+	beam_tween.tween_property(beam_mesh, "rotation:z", rads, duration)\
+				.set_ease(Tween.EASE_IN_OUT)
 
 
 func update_labels() -> void:
