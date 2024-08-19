4f6336746d5c3aecf41706e76259aac254ae6ff0
MrTekTiteToe <tektitetoe@gmail.com>
2024-08-19 15:45:30 -0700

    add credit for fire crackle

--- scene/ui/credits/credits_data.gd
+++ scene/ui/credits/credits_data.gd
@@ -1,3 +1,4 @@
 const third_party_assets = [
-	["Particle VFX textures", "https://www.kenney.nl/assets/particle-pack"]
+	["Particle VFX textures", "https://www.kenney.nl/assets/particle-pack"],
+	["Fire Crackle SFX - TheWoodlandNomad", "https://freesound.org/people/TheWoodlandNomad/sounds/363093/"]
 ]
