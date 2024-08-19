17bc621263c0002675f8cc79de28c9d5a714eaaf
efevans <efevans1@gmail.com>
2024-08-18 22:07:33 -0700

    Add a bit of teetering to the scales

--- scene/scale/scale.gd
+++ scene/scale/scale.gd
@@ -31,18 +31,36 @@ func on_weight_submitted(result_callback: Callable) -> void:
 	var left_end_pos := Vector3(left_pan_mesh.global_position.x, starting_pan_height + pan_height_change, left_pan_mesh.global_position.z)
 	var right_end_pos := Vector3(right_pan_mesh.global_position.x, starting_pan_height - pan_height_change, right_pan_mesh.global_position.z)
 
-	var left_tween := create_tween()
 	var tween_time := 3 if pan_difference != last_pan_difference else 0
-	left_tween.bind_node(left_pan_mesh).tween_property(left_pan_mesh, "global_position", left_end_pos, 3).set_ease(Tween.EASE_OUT)
-	create_tween().bind_node(right_pan_mesh).tween_property(right_pan_mesh, "global_position", right_end_pos, 3).set_ease(Tween.EASE_OUT)
+
+	var left_tween := create_tween().bind_node(left_pan_mesh)
+	var right_tween := create_tween().bind_node(right_pan_mesh)
+
+	# Teeter the pans first to give them some motion
+	teeter_pans(left_tween, right_tween)
+
+	left_tween.tween_property(left_pan_mesh, "global_position", left_end_pos, 3).set_ease(Tween.EASE_OUT)
+	right_tween.tween_property(right_pan_mesh, "global_position", right_end_pos, 3).set_ease(Tween.EASE_OUT)
 	left_tween.tween_callback(func() -> void: result_callback.call())
 	last_pan_difference = pan_difference
-	#left_pan_weight = 0
-	#right_pan_weight = 0
+
 	if (pan_difference == 0):
 		left_tween.tween_callback(func() -> void: GameEvents.emit_correct_weight_submitted())
 	else:
 		left_tween.tween_callback(func() -> void: GameEvents.emit_incorrect_weight_submitted())
+
+
+func teeter_pans(left_tween: Tween, right_tween: Tween) -> void:
+	var left_pan_mesh := $Visuals/PanR as Node3D
+	var right_pan_mesh := $Visuals/PanL as Node3D
+
+	left_tween.tween_property(left_pan_mesh, "global_position", left_pan_mesh.global_position + Vector3.UP * .01, .4)
+	left_tween.tween_property(left_pan_mesh, "global_position", left_pan_mesh.global_position + Vector3.UP * -.01, .6)
+	left_tween.tween_property(left_pan_mesh, "global_position", left_pan_mesh.global_position + Vector3.UP * 0, .4)
+
+	right_tween.tween_property(right_pan_mesh, "global_position", right_pan_mesh.global_position + Vector3.UP * -.01, .4)
+	right_tween.tween_property(right_pan_mesh, "global_position", right_pan_mesh.global_position + Vector3.UP * .01, .6)
+	right_tween.tween_property(right_pan_mesh, "global_position", right_pan_mesh.global_position + Vector3.UP * 0, .4)
 
 
 
